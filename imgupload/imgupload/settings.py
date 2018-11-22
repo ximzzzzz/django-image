688aa72c66abb859412f0ce466fedd7ce47db532
ximzzzzz <ximzzzzz@gmail.com>
2018-11-22 02:29:04 +0000

    image projection

--- imgupload/imgupload/settings.py
+++ imgupload/imgupload/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'imgupload.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates') ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Media files
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
--- imgupload/posts/models.py
+++ imgupload/posts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    image = models.ImageField()
+    content = models.TextField()
+    
+    def __str__(self):
+        return self.content
